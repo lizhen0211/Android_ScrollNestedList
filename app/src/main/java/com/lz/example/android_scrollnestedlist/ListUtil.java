15fff1f91753d36b93b61ddd6093c28840bb0584
lizhen <lizhen@zhumengyuan.com>
2017-04-11 16:39:15 +0800

    add expandablelistview

--- app/src/main/java/com/lz/example/android_scrollnestedlist/ListUtil.java
+++ app/src/main/java/com/lz/example/android_scrollnestedlist/ListUtil.java
@@ -1,6 +1,9 @@
 package com.lz.example.android_scrollnestedlist;
 
+import android.os.Handler;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 
 /**
@@ -12,9 +15,50 @@ public class ListUtil {
     public static void setListViewHeight(int size, int itemHeight, ListView listView) {
 
         ViewGroup.LayoutParams layoutParams = listView.getLayoutParams();
-        layoutParams.height = size
-                * itemHeight
-                + (size - 1)
-                * listView.getDividerHeight();
+        layoutParams.height = size * itemHeight
+                + (size - 1) * listView.getDividerHeight();
+    }
+
+    /**
+     * 处理一级菜单 展开或关闭
+     *
+     * @param groupPosition        当前点击的索引
+     * @param lastOpenGroupPostion 上次点击的索引
+     * @param list
+     */
+    public static void disposeGroupStatus(int groupPosition, int lastOpenGroupPostion, ExpandableListView list) {
+        if (lastOpenGroupPostion == -1) {
+            // 展开被选的group
+            list.expandGroup(groupPosition);
+            // 设置被选中的group置于顶端
+            list.setSelectedGroup(groupPosition);
+        } else if (lastOpenGroupPostion == groupPosition) {
+            if (list.isGroupExpanded(groupPosition)) {
+                list.collapseGroup(groupPosition);
+            } else {
+                list.expandGroup(groupPosition);
+                list.setSelectedGroup(groupPosition);
+            }
+        } else {
+            list.collapseGroup(lastOpenGroupPostion);
+            // 展开被选的group
+            list.expandGroup(groupPosition);
+            // 设置被选中的group置于顶端
+            list.setSelectedGroup(groupPosition);
+            lastOpenGroupPostion = groupPosition;
+        }
+    }
+
+    public static void scrollToTop(final View scroll, final View inner, final int offSet) {
+        Handler mHandler = new Handler();
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (null == scroll || null == inner) {
+                    return;
+                }
+                scroll.scrollTo(0, offSet);
+            }
+        });
     }
 }
--- app/src/main/java/com/lz/example/android_scrollnestedlist/NestedExpandListViewActivity.java
+++ app/src/main/java/com/lz/example/android_scrollnestedlist/NestedExpandListViewActivity.java
@@ -2,28 +2,88 @@ package com.lz.example.android_scrollnestedlist;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ExpandableListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class NestedExpandListViewActivity extends Activity {
 
+    private ExpandableListView expandableListView;
+
+    private NestedExpandListViewAdapter expandListViewAdapter;
+
+    private TextView titleView;
+
+    private View titleDividerView;
+
+    private int listItemHeight;
+
+    private int lastOpenGroupPostion = -1;
+
+    private ScrollView scrollView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nested_expand_list_view);
-        ExpandableListView expandableListView = (ExpandableListView) findViewById(R.id.expandable_list_view);
+        expandableListView = (ExpandableListView) findViewById(R.id.expandable_list_view);
+        scrollView = (ScrollView) findViewById(R.id.scrollview);
+        titleView = (TextView) findViewById(R.id.title_view);
+        titleDividerView = findViewById(R.id.title_divider);
         expandableListView.setGroupIndicator(null);
         expandableListView.setChildIndicator(null);
         expandableListView.setScrollingCacheEnabled(false);
         expandableListView.setDrawingCacheEnabled(false);
 
-        NestedExpandListViewAdapter expandListViewAdapter = new NestedExpandListViewAdapter(this, getData());
+        expandableListView.setOnGroupExpandListener(onGroupExpandListener);
+        expandableListView.setOnGroupCollapseListener(onGroupCollapseListener);
+        expandableListView.setOnGroupClickListener(onGroupClickListener);
+
+        expandListViewAdapter = new NestedExpandListViewAdapter(this, getData());
         expandableListView.setAdapter(expandListViewAdapter);
+        listItemHeight = (int) getResources().getDimension(R.dimen.expand_list_item_height);
         ListUtil.setListViewHeight(expandListViewAdapter.getGroupCount()
                 , (int) getResources().getDimension(R.dimen.expand_list_item_height), expandableListView);
     }
+
+    private ExpandableListView.OnGroupExpandListener onGroupExpandListener = new ExpandableListView.OnGroupExpandListener() {
+        @Override
+        public void onGroupExpand(int groupPosition) {
+            lastOpenGroupPostion = groupPosition;
+            //展开时，listview 高度重新计算后，高度错误。
+            //需要再次设置正确的listview高度。
+            ListUtil.setListViewHeight(expandListViewAdapter.getGroupCount(), listItemHeight, expandableListView);
+        }
+    };
+
+    private ExpandableListView.OnGroupCollapseListener onGroupCollapseListener = new ExpandableListView.OnGroupCollapseListener() {
+        @Override
+        public void onGroupCollapse(int groupPosition) {
+            lastOpenGroupPostion = -1;
+            ListUtil.setListViewHeight(expandListViewAdapter.getGroupCount(), listItemHeight, expandableListView);
+        }
+    };
+
+    private ExpandableListView.OnGroupClickListener onGroupClickListener = new ExpandableListView.OnGroupClickListener() {
+        @Override
+        public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+            //手动展开与关闭groupItem
+            ListUtil.disposeGroupStatus(groupPosition, lastOpenGroupPostion, expandableListView);
+
+           /* int dividerHeight = expandableListView.getDividerHeight();
+            int height = (dividerHeight + listItemHeight) * groupPosition;
+            int scrollTo = height + titleView.getHeight() + titleDividerView.getHeight();
+            //设置滚到位置
+            ListUtil.scrollToTop(scrollView, expandableListView, scrollTo);*/
+
+            // 返回true 展开与关闭要自己处理；返回false展开关闭由系统处理
+            return true;
+        }
+    };
 
     private List<NestedExpandListViewAdapter.DataEntry> getData() {
         List<NestedExpandListViewAdapter.DataEntry> dataEntries = new ArrayList<>();
