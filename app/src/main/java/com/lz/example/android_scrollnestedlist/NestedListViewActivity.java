cd456e36afef05cd4d1e889b4f5c32f1fee579ba
lizhen <lizhen@zhumengyuan.com>
2017-04-11 11:15:50 +0800

    base line

--- app/src/main/java/com/lz/example/android_scrollnestedlist/NestedListViewActivity.java
+++ app/src/main/java/com/lz/example/android_scrollnestedlist/NestedListViewActivity.java
@@ -3,29 +3,28 @@ package com.lz.example.android_scrollnestedlist;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.ScrollView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class NestedListViewActivity extends Activity {
 
-    private ListView listOne;
-
-    private NestedListViewAdapter adapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nested_list_view);
-        listOne = (ListView) findViewById(R.id.listOne);
+        ListView listOne = (ListView) findViewById(R.id.listOne);
         listOne.setScrollingCacheEnabled(false);
         listOne.setDrawingCacheEnabled(false);
-        adapter = new NestedListViewAdapter(this, getData());
-        listOne.setAdapter(adapter);
-        setListViewHeight();
+        NestedListViewAdapter listOneAdapter = new NestedListViewAdapter(this, getData());
+        listOne.setAdapter(listOneAdapter);
+        setListViewHeight(listOneAdapter.getDatas().size(), listOne);
+
+        ListView listTwo = (ListView) findViewById(R.id.listTwo);
+        NestedListViewAdapter listTwoAdapter = new NestedListViewAdapter(this, getData());
+        listTwo.setAdapter(listTwoAdapter);
+        setListViewHeight(listTwoAdapter.getDatas().size(), listTwo);
     }
 
     private List<String> getData() {
@@ -36,13 +35,12 @@ public class NestedListViewActivity extends Activity {
         return list;
     }
 
-    private void setListViewHeight() {
-        List<String> data = adapter.getDatas();
-        ViewGroup.LayoutParams layoutParams = listOne.getLayoutParams();
-        layoutParams.height = data.size()
-                * (int) getResources().getDimension(R.dimen.list_item_height)
-                + (data.size() - 1)
-                * listOne.getDividerHeight();
-    }
+    private void setListViewHeight(int size, ListView listView) {
 
+        ViewGroup.LayoutParams layoutParams = listView.getLayoutParams();
+        layoutParams.height = size
+                * (int) getResources().getDimension(R.dimen.list_item_height)
+                + (size - 1)
+                * listView.getDividerHeight();
+    }
 }
